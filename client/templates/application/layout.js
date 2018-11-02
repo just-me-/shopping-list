836c4e4747f93d59b6b13d613f2db272962d72aa
just-me- <hess-marcel@bluewin.ch>
2018-11-02 19:53:52 +0100

    Online status of users

--- client/templates/application/layout.js
+++ client/templates/application/layout.js
@@ -1,3 +1,16 @@
 Template.layout.helpers( {
   version: function() { return Version.findOne(); }
 });
+
+Meteor.users.find({ "status.online": true }).observe({
+  added: function(user) {
+    // id just came online
+    if(Meteor.user()._id != user._id) {
+      console.log(user.profile.first_name+"on");
+    }
+  },
+  removed: function(user) {
+    // id just went offline
+    console.log(user.profile.first_name+"off");
+  }
+});
